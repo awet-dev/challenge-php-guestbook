26c6834319c876ad4cf04c06ebf8c6c2879a3a03
awet tesfay <whitezscope@gmail.com>
2020-10-15 10:29:49 +0200

    add the base files

---
+++ Postloader.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+class PostLoader {
+
+}
+
+class Post {
+    private $Title;
+    private $Date;
+    private $Content;
+    private $AuthorName;
+
+    function __construct () {
+
+    }
+}
