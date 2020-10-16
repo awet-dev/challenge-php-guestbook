ebe115fc8334228706a4a4e9d1067f72ee18bc5c
awet tesfay <whitezscope@gmail.com>
2020-10-16 15:44:54 +0200

    retrieve data and display to the page

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ error_reporting(E_ALL);
 
 require "Postloader.php";
 require "Post.php";
-require "post.json";
 
 if(!isset($_POST['title'])) {
     $_POST['title'] = "";
@@ -31,6 +30,28 @@ if (isset($_POST['send'])) {
     $loader = new Postloader($title, $time, $content, $name);
 }
 
+function retrieveData() {
+    if (!empty(file_get_contents("post.json"))) {
+        $data = file_get_contents("post.json");
+        $data = json_decode($data, true);
+        return $data;
+    } else {
+        return 0;
+    }
+}
+
+$display = array();
+$postData = retrieveData();
+if (isset($_POST["Check"])) {
+    if (isset($_POST['visitors']) && is_numeric($_POST['visitors']) && $postData) {
+        $display = array_slice($postData, -$_POST['visitors'], $_POST['visitors']);
+        $display = array_reverse($display);
+    }
+} elseif (isset($_POST['send'])) {
+    $display = array_slice($postData, -20, 20);
+    $display = array_reverse($display);
+}
+
 ?>
 
 <!doctype html>
@@ -47,13 +68,13 @@ if (isset($_POST['send'])) {
 
 <nav class="navbar navbar-light bg-light mb-3 justify-content-*-between">
     <a class="navbar-brand" href="#">Guest Book</a>
-    <form action="index.php" method="post" class="form-inline my-2 my-lg-0">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="form-inline my-2 my-lg-0">
         <input name="visitors" class="form-control mr-sm-2" type="tel" placeholder="Number Of Visitor" aria-label="Search">
         <input name="Check" class="btn btn-outline-success my-2 my-sm-0" type="submit" value="Check Visitors">
     </form>
 </nav>
 
-<form action="index.php" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <div class="form-group">
         <input name="name" type="text" class="form-control mb-2" id="inputEmail4" placeholder="Guest Name">
         <input name="title" type="text" class="form-control mb-2" id="inputZip" placeholder="Title">
@@ -61,7 +82,15 @@ if (isset($_POST['send'])) {
     </div>
     <input name="send" type="submit" class="btn btn-primary" value="Send"/>
 </form>
-
+    <?php foreach ($display as $post)  :?>
+        <div class='card'>
+            <div class='card-body'>
+                <h4 class='card-title'><?php echo "Title: ".$post["title"]?></h4>
+                <h6 class='card-subtitle mb-2 text-muted'><?php echo "Name: ".$post["name"] ."  visit at: " .$post["time"]?></h6>
+                <p class='card-text'><?php echo "Comments: ".$post["content"]?></p>
+            </div>
+        </div>
+    <?php endforeach;?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
